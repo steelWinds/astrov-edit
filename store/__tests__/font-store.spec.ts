bdcff118e45abcd157b773dd77e4aaed9cd7a5ef
steelWinds <kirillsurov0@gmail.com>
2023-03-03 01:21:03 +0900

    fix(tests): add skip test

--- store/__tests__/font-store.spec.ts
+++ store/__tests__/font-store.spec.ts
@@ -1,1 +1,13 @@
-// TODO: Add test
+import { describe, test, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+// import { useFilesStore } from '@/store/files-store'
+
+describe.skip('Spec of pinia file-store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  test.skip('Fix me', () => {
+
+  })
+})
